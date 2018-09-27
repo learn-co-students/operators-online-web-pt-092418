efd4d070d4eff87168e4f0bd1a1dc7b757d7bf98
embeor <embeor@gmail.com>
2018-09-27 02:38:03 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+	if speed < 40
+		!!speed
+	elsif speed > 60
+		!!speed
+	else
+		!speed
+	end
 end
-
-
 
 def not_safe?(speed)
-	
+	speed == >40 <60 ? !!speed:!speed
+
 end
-	
-
-
